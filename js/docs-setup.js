fb95d17e2f0168b294b1f91286bb6c1c6065bd41
Dallan Quass <dallan@quass.org>
2013-11-08 12:21:06 -0600

    update docs

--- js/docs-setup.js
+++ js/docs-setup.js
@@ -12,122 +12,157 @@ NG_DOCS={
     },
     {
       "section": "api",
-      "id": "api",
-      "shortName": "api",
+      "id": "authentication",
+      "shortName": "authentication",
       "type": "overview",
-      "keywords": "api are functions overview the these"
+      "keywords": "an api are authentication authorization call code do docs familysearch function functions get getaccesstoken getauthcode https if in is it main not one org overview pass the these to will you"
     },
     {
       "section": "api",
-      "id": "api.functions:getAncestry",
-      "shortName": "getAncestry",
-      "type": "function",
-      "keywords": "ancestors ancestry and api during example for function functions generations get http https id init number of optional optionally options opts org pass person promise retrieve specified spouse spouseid the to"
-    },
-    {
-      "section": "api",
-      "id": "api.functions:getCurrentUser",
-      "shortName": "getCurrentUser",
-      "type": "function",
-      "keywords": "api current during example familysearch for function functions get http https init net optional options opts org pass promise specified the to user"
-    },
-    {
-      "section": "api",
-      "id": "api.functions:getMultiPerson",
-      "shortName": "getMultiPerson",
-      "type": "function",
-      "keywords": "all an api array been during fulfilled function functions get have http id ids init is map of optional options opts pass people person promise read response returning specified that the to when"
-    },
-    {
-      "section": "api",
-      "id": "api.functions:getPerson",
-      "shortName": "getPerson",
-      "type": "function",
-      "keywords": "api during example for function functions get http https id init of optional options opts org pass person promise read specified the to"
-    },
-    {
-      "section": "api",
-      "id": "auth",
-      "shortName": "auth",
-      "type": "overview",
-      "keywords": "api are auth authentication functions overview the these"
-    },
-    {
-      "section": "api",
-      "id": "auth.functions:getAccessToken",
+      "id": "authentication.functions:getAccessToken",
       "shortName": "getAccessToken",
       "type": "function",
-      "keywords": "access any api auth authcode authentication before by caches call calls code don ensure first for from function functions get getauthcode if in is just making need not of optional passed promise require resolves returned sdk so that the this to token user will you"
+      "keywords": "access any api auth authcode authentication before by call calls code docs don editable ensure example familysearch for from function functions get getauthcode http https if in just making need net not of org passed promise requests require resolves returned store that the this to token user will you"
     },
     {
       "section": "api",
-      "id": "auth.functions:getAuthCode",
+      "id": "authentication.functions:getAuthCode",
       "shortName": "getAuthCode",
       "type": "function",
-      "keywords": "allow and api application auth authenticate authorize code function functions of open popup promise the this to user window"
+      "keywords": "allow an and api application auth authenticate authentication authorization authorize call code do docs familysearch function functions get getaccesstoken have https if in not of one open org passing popup promise that the this to user will window without you"
     },
     {
       "section": "api",
-      "id": "auth.functions:invalidateAccessToken",
+      "id": "authentication.functions:invalidateAccessToken",
       "shortName": "invalidateAccessToken",
       "type": "function",
-      "keywords": "access api auth been current function functions has invalidate invalidated is once promise resolved that the token"
+      "keywords": "access api authentication been current function functions has invalidate invalidated is once promise resolved that the token"
     },
     {
       "section": "api",
       "id": "init",
       "shortName": "init",
       "type": "overview",
-      "keywords": "api call familysearch function init initialize object once overview the to"
+      "keywords": "any api before call calling familysearch function functions init initialize object once other overview the to"
     },
     {
       "section": "api",
       "id": "init.functions:init",
       "shortName": "init",
       "type": "function",
-      "keywords": "$http $q access access_token ajax already an and angular api app_key as auth_callback auto_signin be browser but call creating currently deferred deferred_function description developer does enable environment eventually exist false familysearch for from function functions have host http http_function if in init initialize issuing jquery js key logging may must need node not oauth2 object options opts or pass popups port production prompted received redirect registered requests require returns running same sandbox script server set sign staging the their this to token true unauthorized uri used user want when with you your"
+      "keywords": "$http $q access access_token ajax already an and angular api app_key as auth_callback auto_signin be browser but call creating currently deferred deferred_function developer does enable environment eventually exist false familysearch for from function functions have host http http_function if in init initialize issuing jquery js key logging may must need node not oauth2 object options opts or pass popups port production prompted received redirect registered requests require returns running same sandbox script server set sign staging the their this to token true unauthorized uri used user want when with you your"
+    },
+    {
+      "section": "api",
+      "id": "pedigree",
+      "shortName": "pedigree",
+      "type": "overview",
+      "keywords": "ancestry api descendancy docs familysearch get https or org overview pedigree someone"
+    },
+    {
+      "section": "api",
+      "id": "pedigree.functions:getAncestry",
+      "shortName": "getAncestry",
+      "type": "function",
+      "keywords": "ahnentafel ancestors ancestry and api convenience docs during editable example exists familysearch following for function functions generations get getgender getgivenname getid getlifespan getname getsurname http https id in init is isliving net number of optionally options opts org pass pedigree person promise retrieve specified spouse spouseid the to tree wikipedia with"
+    },
+    {
+      "section": "api",
+      "id": "person",
+      "shortName": "person",
+      "type": "overview",
+      "keywords": "api docs familysearch functions https org overview person persons related to"
+    },
+    {
+      "section": "api",
+      "id": "person.functions:getMultiPerson",
+      "shortName": "getMultiPerson",
+      "type": "function",
+      "keywords": "all api at been by docs during editable example familysearch fulfilled function functions get have http https id ids in init is map multiple net of once options opts org parallel pass people person promise read requesting response returning specified that the them to when"
+    },
+    {
+      "section": "api",
+      "id": "person.functions:getPerson",
+      "shortName": "getPerson",
+      "type": "function",
+      "keywords": "api convenience docs during editable example familysearch following for function functions get getbirthdate getbirthplace getdeathdate getdeathplace getgender getgivenname getid getlifespan getname getsurname http https id init isliving net of options opts org pass person promise read specified the to with"
     },
     {
       "section": "api",
       "id": "plumbing",
       "shortName": "plumbing",
       "type": "overview",
-      "keywords": "api are functions overview plumbing the these"
+      "keywords": "api are don functions low-level need normally overview plumbing the these to use you"
     },
     {
       "section": "api",
       "id": "plumbing.functions:del",
       "shortName": "del",
       "type": "function",
-      "keywords": "api be behaves by call data delete during endpoint extend familysearch from function functions headers http init like low-level may optional options opts pass plumbing promise promises relative response responsedataextender rest returned specific specified that the to url"
+      "keywords": "api be behaves by call data delete during endpoint extend familysearch from function functions headers http init like low-level may options opts pass plumbing promise promises relative response responsedataextender rest returned specific specified that the to url"
     },
     {
       "section": "api",
       "id": "plumbing.functions:get",
       "shortName": "get",
       "type": "function",
-      "keywords": "api be behaves by call data during endpoint extend familysearch from function functions get headers http init like low-level may optional options opts parameters params pass plumbing promise promises query relative response responsedataextender rest returned specific specified that the to url"
+      "keywords": "api be behaves by call data during endpoint extend familysearch from function functions get headers http init like low-level may options opts parameters params pass plumbing promise promises query relative response responsedataextender rest returned specific specified that the to url"
+    },
+    {
+      "section": "api",
+      "id": "plumbing.functions:getTotalProcessingTime",
+      "shortName": "getTotalProcessingTime",
+      "type": "function",
+      "keywords": "api endpoints familysearch function functions in milliseconds plumbing processing rest return spent the time total"
     },
     {
       "section": "api",
       "id": "plumbing.functions:http",
       "shortName": "http",
       "type": "function",
-      "keywords": "an api be behaves by call data delete during endpoint extend familysearch from function functions get headers http init issue like low-level may method number object of optional options opts or pass plumbing post promise promises put relative request response responsedataextender rest retries retry returned specific specified that the times to url"
+      "keywords": "an api be behaves by call data delete during endpoint extend familysearch from function functions get headers http init issue like low-level may method number object of options opts or pass plumbing post promise promises put relative request response responsedataextender rest retries retry returned specific specified that the times to url"
     },
     {
       "section": "api",
       "id": "plumbing.functions:post",
       "shortName": "post",
       "type": "function",
-      "keywords": "api be behaves by call data during endpoint extend familysearch from function functions headers http init like low-level may optional options opts pass plumbing post promise promises relative response responsedataextender rest returned specific specified that the to url"
+      "keywords": "api be behaves by call data during endpoint extend familysearch from function functions headers http init like low-level may options opts pass plumbing post promise promises relative response responsedataextender rest returned specific specified that the to url"
     },
     {
       "section": "api",
       "id": "plumbing.functions:put",
       "shortName": "put",
       "type": "function",
-      "keywords": "api be behaves by call data during endpoint extend familysearch from function functions headers http init like low-level may optional options opts pass plumbing post promise promises put relative response responsedataextender rest returned specific specified that the to url"
+      "keywords": "api be behaves by call data during endpoint extend familysearch from function functions headers http init like low-level may options opts pass plumbing post promise promises put relative response responsedataextender rest returned specific specified that the to url"
+    },
+    {
+      "section": "api",
+      "id": "plumbing.functions:setTotalProcessingTime",
+      "shortName": "setTotalProcessingTime",
+      "type": "function",
+      "keywords": "api could counter endpoints familysearch function functions if in milliseconds plumbing processing reset rest set spent the this time to use wanted you zero"
+    },
+    {
+      "section": "api",
+      "id": "user",
+      "shortName": "user",
+      "type": "overview",
+      "keywords": "api docs familysearch functions https org overview related to user users"
+    },
+    {
+      "section": "api",
+      "id": "user.functions:getCurrentUser",
+      "shortName": "getCurrentUser",
+      "type": "function",
+      "keywords": "api convenience current docs during editable example familysearch following for function functions get getcontactname getid gettreeuserid http https init net options opts org pass promise specified the to user with"
+    },
+    {
+      "section": "api",
+      "id": "user.functions:getCurrentUserPerson",
+      "shortName": "getCurrentUserPerson",
+      "type": "function",
+      "keywords": "api current docs during editable example familysearch for function functions get http https id in init net of options opts org pass person promise specified the to tree user"
     }
   ],
   "apis": {
