d1a6e3d0a609d7370b25c8cf676c0c30b114b66c
Dallan Quass <dallan@quass.org>
2013-11-07 18:01:27 -0600

    use grunt-ngdocs for documentation

---
+++ js/docs-setup.js
@@ -0,0 +1,113 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "api",
+      "shortName": "api",
+      "type": "overview",
+      "keywords": "api are functions overview the these"
+    },
+    {
+      "section": "api",
+      "id": "api.functions:getAccessToken",
+      "shortName": "getAccessToken",
+      "type": "function",
+      "keywords": "access any api auth authcode authentication before by caches call calls code don ensure first for from function functions get getauthcode if in is just making need not of optional passed promise require resolves returned sdk so that the this to token user will you"
+    },
+    {
+      "section": "api",
+      "id": "api.functions:getAncestry",
+      "shortName": "getAncestry",
+      "type": "function",
+      "keywords": "ancestors ancestry and api during example for function functions generations get http https id init number of optional optionally options opts org pass person promise retrieve specified spouse spouseid the to"
+    },
+    {
+      "section": "api",
+      "id": "api.functions:getAuthCode",
+      "shortName": "getAuthCode",
+      "type": "function",
+      "keywords": "allow and api application auth authenticate authorize code function functions of open popup promise the this to user window"
+    },
+    {
+      "section": "api",
+      "id": "api.functions:getCurrentUser",
+      "shortName": "getCurrentUser",
+      "type": "function",
+      "keywords": "api current during example familysearch for function functions get http https init net optional options opts org pass promise specified the to user"
+    },
+    {
+      "section": "api",
+      "id": "api.functions:getMultiPerson",
+      "shortName": "getMultiPerson",
+      "type": "function",
+      "keywords": "all an api array been during fulfilled function functions get have http id ids init is map of optional options opts pass people person promise read response returning specified that the to when"
+    },
+    {
+      "section": "api",
+      "id": "api.functions:getPerson",
+      "shortName": "getPerson",
+      "type": "function",
+      "keywords": "api during example for function functions get http https id init of optional options opts org pass person promise read specified the to"
+    },
+    {
+      "section": "api",
+      "id": "api.functions:init",
+      "shortName": "init",
+      "type": "function",
+      "keywords": "$http $q access access_token ajax already an and angular api app_key as auth_callback auto_signin be browser but call creating currently deferred deferred_function description developer does enable environment eventually exist false familysearch for from function functions have host http http_function if in initialize issuing jquery js key logging may must need node not oauth2 object options opts or pass popups port production prompted received redirect registered requests require returns running same sandbox script server set sign staging the their this to token true unauthorized uri used user want when with you your"
+    },
+    {
+      "section": "api",
+      "id": "plumbing",
+      "shortName": "plumbing",
+      "type": "overview",
+      "keywords": "api are functions overview plumbing the these"
+    },
+    {
+      "section": "api",
+      "id": "plumbing.functions:del",
+      "shortName": "del",
+      "type": "function",
+      "keywords": "api be behaves by call data delete during endpoint extend familysearch from function functions headers http init like low-level may optional options opts pass plumbing promise promises relative response responsedataextender rest returned specific specified that the to url"
+    },
+    {
+      "section": "api",
+      "id": "plumbing.functions:get",
+      "shortName": "get",
+      "type": "function",
+      "keywords": "api be behaves by call data during endpoint extend familysearch from function functions get headers http init like low-level may optional options opts parameters params pass plumbing promise promises query relative response responsedataextender rest returned specific specified that the to url"
+    },
+    {
+      "section": "api",
+      "id": "plumbing.functions:http",
+      "shortName": "http",
+      "type": "function",
+      "keywords": "an api be behaves by call data delete during endpoint extend familysearch from function functions get headers http init issue like low-level may method number object of optional options opts or pass plumbing post promise promises put relative request response responsedataextender rest retries retry returned specific specified that the times to url"
+    },
+    {
+      "section": "api",
+      "id": "plumbing.functions:post",
+      "shortName": "post",
+      "type": "function",
+      "keywords": "api be behaves by call data during endpoint extend familysearch from function functions headers http init like low-level may optional options opts pass plumbing post promise promises relative response responsedataextender rest returned specific specified that the to url"
+    },
+    {
+      "section": "api",
+      "id": "plumbing.functions:put",
+      "shortName": "put",
+      "type": "function",
+      "keywords": "api be behaves by call data during endpoint extend familysearch from function functions headers http init like low-level may optional options opts pass plumbing post promise promises put relative response responsedataextender rest returned specific specified that the to url"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
