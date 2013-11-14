20b3415d6050b00edda59fe29841434881a42880
Dallan Quass <dallan@quass.org>
2013-11-13 22:05:58 -0600

    clean contents of docs directory but dont delete docs directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     clean: {
-      docs: ['docs']
+      docs: ['docs/*']
     },
     ngdocs: {
       options: {
