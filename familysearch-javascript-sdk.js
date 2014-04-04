2ccaeb030e3564461b003b49b1cba611a8cc4a13
Travis <travis@travis-ci.org>
2014-04-04 21:42:37 +0000

    Update docs and files to distribute

--- familysearch-javascript-sdk.js
+++ familysearch-javascript-sdk.js
@@ -1285,7 +1285,7 @@ define('jquery-wrappers',[
         },
         function(jqXHR, textStatus, errorThrown) {
           statusCode = jqXHR.status;
-          if (!jqXHR.responseText) {
+          if (statusCode >= 200 && statusCode <= 299 && !jqXHR.responseText) {
             // FamilySearch sometimes returns no content in the response even though we have requested json
             // No content is not valid json, so we get an error parsing it
             // Treat it as valid but empty content
