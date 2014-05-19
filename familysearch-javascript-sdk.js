b09bb1439269763305a8a902f6dd015a5f49da21
Travis <travis@travis-ci.org>
2014-05-19 17:40:11 +0000

    Update docs and files to distribute

--- familysearch-javascript-sdk.js
+++ familysearch-javascript-sdk.js
@@ -4270,7 +4270,12 @@ define('name',[
     $setFullText: function(fullText, i) {
       this.$changed = true;
       var nameForm = ensureNameForm(this, i);
-      nameForm.fullText = fullText;
+      if (!!fullText) {
+        nameForm.fullText = fullText;
+      }
+      else {
+        delete nameForm.fullText;
+      }
       //noinspection JSValidateTypes
       return this;
     },
@@ -4294,11 +4299,16 @@ define('name',[
         nameForm.parts = [];
       }
       var part = helpers.find(nameForm.parts, {type: type});
-      if (!part) {
-        part = {type: type};
-        nameForm.parts.push(part);
+      if (!!name) {
+        if (!part) {
+          part = {type: type};
+          nameForm.parts.push(part);
+        }
+        part.value = name;
       }
-      part.value = name;
+      else if (!!part) {
+        nameForm.parts.splice(nameForm.parts.indexOf(part), 1);
+      }
       //noinspection JSValidateTypes
       return this;
     },
