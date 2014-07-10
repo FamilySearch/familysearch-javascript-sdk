67f4c9a859498c29e43f72175e0b307209c99f3e
Travis <travis@travis-ci.org>
2014-07-10 22:51:59 +0000

    Update docs and files to distribute

--- familysearch-javascript-sdk.js
+++ familysearch-javascript-sdk.js
@@ -1168,13 +1168,16 @@ define('helpers',[
   /**
    * Erase access token, clear the expiry timers, and erase the cookie
    */
-  exports.eraseAccessToken = function() {
+  exports.eraseAccessToken = function(omitCallback) {
     globals.accessToken = null;
     if (globals.autoExpire) {
       clearAccessTokenTimers();
     }
     if (globals.saveAccessToken) {
       exports.eraseCookie(globals.accessTokenCookie);
+    }
+    if (!!globals.expireCallback && !omitCallback) {
+      globals.expireCallback();
     }
   };
 
@@ -1721,14 +1724,15 @@ define('init',[
    * - `deferred_function` - a function for creating deferred's: `jQuery.Deferred` or angular's `$q.defer`
    * or eventually `Q`; defaults to `jQuery.Deferred`
    * - `timeout_function` - optional timeout function: angular users should pass `$timeout`; otherwise the global `setTimeout` is used
-   * - `auth_callback` - the OAuth2 redirect uri you registered with FamilySearch.  Does not need to exist,
+   * - `redirect_uri` - the OAuth2 redirect uri you registered with FamilySearch.  Does not need to exist,
    * but must have the same host and port as the server running your script;
    * however, it must exist for mobile safari - see the Overview section of the documentation
    * - `auto_expire` - set to true if you want to the system to clear the access token when it has expired
    * (after one hour of inactivity or 24 hours, whichever comes first; should probably be false for node.js)
    * - `auto_signin` - set to true if you want the user to be prompted to sign in whenever you call an API function
-   * without an access token (must be false for node.js, and may result in a blocked pop-up if the API call is
-   * not in direct response to a user-initiated action)
+   * without an access token; must be false for node.js, and may result in a blocked pop-up if the API call is
+   * not in direct response to a user-initiated action; because of the blocked pop-up issue, you may want to use `expire_callback` instead
+   * - `expire_callback` - pass in a function that will be called when the access token expires
    * - `save_access_token` - set to true if you want the access token to be saved and re-read in future init calls
    * (uses a session cookie, must be false for node.js) - *setting `save_access_token` along with `auto_signin` and
    * `auto_expire` is very convenient*
@@ -1802,6 +1806,8 @@ define('init',[
     globals.autoSignin = opts['auto_signin'];
 
     globals.autoExpire = opts['auto_expire'];
+
+    globals.expireCallback = opts['expire_callback'];
 
     if (opts['save_access_token']) {
       globals.saveAccessToken = true;
@@ -2012,7 +2018,7 @@ define('authentication',[
    * @return {Object} promise that is resolved once the access token has been invalidated
    */
   exports.invalidateAccessToken = function() {
-    helpers.eraseAccessToken();
+    helpers.eraseAccessToken(true);
     return helpers.chainHttpPromises(
       plumbing.getUrl('http://oauth.net/core/2.0/endpoint/token'),
       function(url) {
