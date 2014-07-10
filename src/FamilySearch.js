e11a72b1ac50e6a57130c8c7763eaf1cf5d7800d
Dallan Quass <dallan@quass.org>
2014-07-10 14:47:47 -0500

    remove extraneous FS.Foo - closes #73

--- src/FamilySearch.js
+++ src/FamilySearch.js
@@ -22,18 +22,7 @@ define([
 ], function(init, authentication, authorities, changeHistory, discussions, fact, helpers, memories, name, notes,
             parentsAndChildren, pedigree, person, plumbing, searchAndMatch, sourceBox, sources, spouses, user, utilities) {
 
-  var Foo = function(params) {
-    //noinspection JSPotentiallyInvalidUsageOfThis
-    this.params = params;
-
-    this.getAccessToken = helpers.partial(authentication.getAccessToken, this.params);
-    this.getCurrentUser = helpers.partial(user.getCurrentUser, this.params);
-    this.Fact = function(data) { return new fact.Fact(this.params, data); };
-    this.createFact = function(data) { return new fact.Fact(this.params, data); };
-  };
-
   return {
-    Foo: Foo,
     init: init.init,
 
     // authentication
