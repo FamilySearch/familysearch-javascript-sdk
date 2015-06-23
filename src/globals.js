d9cc44e73725049bf02a874ace8a351d39da0fff
Justin <justincyork@gmail.com>
2015-06-23 19:11:52 +0000

    fix auto signing bug for sandbox; update examples

--- src/globals.js
+++ src/globals.js
@@ -31,7 +31,7 @@ module.exports = {
     'production': 'https://familysearch.org'
   },
   oauthServer: {
-    'sandbox'   : 'https://sandbox.familysearch.org/cis-web/oauth2/v3',
+    'sandbox'   : 'https://integration.familysearch.org/cis-web/oauth2/v3',
     'staging'   : 'https://identbeta.familysearch.org/cis-web/oauth2/v3',
     'beta'      : 'https://identbeta.familysearch.org/cis-web/oauth2/v3',
     'production': 'https://ident.familysearch.org/cis-web/oauth2/v3'
